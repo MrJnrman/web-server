a11c091b4848f015847713f4821c79166f79cf4a
Jamel Reid <jnoelreid@gmail.com>
2016-10-30 20:34:17 -0500

    Updated Language

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.listen(PORT, function() {
-	console.log('Express server running on ' + PORT);
+	console.log('Express server running on ' + PORT + '!');
 });
